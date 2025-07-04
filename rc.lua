41123828e8ca80f7b2b20c3c1c2c3b7c67740c68
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-04 10:08:23 -0400

    Put the wibar on the bottom and make it visible.

--- rc.lua
+++ rc.lua
@@ -208,7 +208,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s, visible = false})
+    s.mywibox = awful.wibar({ position = "bottom", screen = s, visible = true})
 
     -- Add widgets to the wibox
     s.mywibox:setup {
