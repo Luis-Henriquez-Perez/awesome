e518ea3a5f005280a744c564d7cedf4b275a0a75
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-04 10:17:00 -0400

    Make the default font bigger.

--- rc.lua
+++ rc.lua
@@ -52,6 +52,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.font = "Sans 14"
 revelation.init()
 
 -- This is used later as the default terminal and editor to run.
