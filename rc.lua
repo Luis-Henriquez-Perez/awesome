437db518a0c6ac2e65aa776fa2dcb80c0d7e0328
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-07 15:29:15 -0400

    Update the awesome menu.

--- rc.lua
+++ rc.lua
@@ -103,7 +103,8 @@ myawesomemenu = {
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
+                                    { "change wallpaper", "wallpaper", "/usr/share/icons/AdwaitaLegacy/32x32/legacy/preferences-desktop-wallpaper.png"},
+                                    { "open terminal", terminal, "/usr/share/icons/AdwaitaLegacy/32x32/legacy/utilities-terminal.png"}
                                   }
                         })
 
