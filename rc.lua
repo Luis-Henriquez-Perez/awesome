3e32ea35816bcc7d629a21f52f0e712903ef2eb5
Luis Henriquez-Perez <luis@luishp.xyz>
2025-07-08 10:08:29 -0400

    Update script call.

--- rc.lua
+++ rc.lua
@@ -371,7 +371,7 @@ local function client_rotate_backward()
 end
 
 local function system_take_screenshot()
-    awful.spawn.with_shell("maim ~/Pictures/screenshot_$(date +%Y%m%d_%H%M%S).png")
+    awful.spawn("screenshot")
     naughty.notify({title = "maim", text = "take screenshot"})
 end
 
